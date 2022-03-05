9537f8369c26a19c3ff99d27e5a301962081f997
Marian <95725615+Mariangdev@users.noreply.github.com>
2022-03-05 17:21:19 -0300

    agregando NavBar simple

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Sidebar() {
+    return(
+        <div>
+            <ul>
+                <li>
+                    <a href="#">Celulares</a>
+                </li>
+                <li>
+                    <a href="#">Tablets</a>
+                </li>
+                <li>
+                    <a href="#">Auriculares</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/components/titulo.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Tittle() {
+    return(
+        <div>
+           <h1>Agod's Store</h1>
+        </div>
+    )
+}
+
+export default Tittle
